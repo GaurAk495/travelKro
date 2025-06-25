474a8ad3901d141027fe198265dda29af4c7d6fb
AkhileshDev <Gaurak495@gmail.com>
2025-06-25 22:29:05 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+function page() {
+  return (
+    <div className="flex-center ">
+      page
+      <button className="button bg-blue-500 text-white">Click Me</button>
+    </div>
+  );
+}
+
+export default page;
